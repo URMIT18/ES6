63ac86537135b76680c82e4d34d6ed97289e07e8
Urmit Tajwala <105055954+URMIT18@users.noreply.github.com>
2024-03-14 12:18:46 +0530

    Add files via upload

--- expressdemo.js
+++ expressdemo.js
@@ -9,10 +9,20 @@ app.get('/home', (req, res) => {
     res.sendFile(__dirname + "/home.html");
 });
 app.get('/about', (req, res) => {
-    res.sendfile(__dirname + "/about.html");
+    res.sendFile(__dirname + "/about.html");
 });
 app.get('/contact', (req, res) => {
     res.sendFile(__dirname + "/contact.html");
+});
+app.get('/search/', (req, res) => {
+    var id = req.query.id;
+    res.send("Sorry " + id + " not found");
+});
+app.get('/process/', (req, res) => {
+    var txt1 = parseInt(req.query.txt1);
+    var txt2 = parseInt(req.query.txt2);
+    var c = txt1 + txt2;
+    res.send("A value is " + txt1 + " B value is " + txt2 + " sum value is " + c);
 });
 
 app.listen(port, () => {
